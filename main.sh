83b8e72731be20360a05f9034d0c581d8784f9a1
Zephic06 <70858757+Zephic06@users.noreply.github.com>
2023-09-26 08:55:32 +0200

    Update and rename main.sh to deploy.yml

--- main.sh
+++
@@ -1,9 +0,0 @@
-npm install -g @vue/cli
-cd src
-npm run build
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/yourusername/App.vue
-git push -u origin main
