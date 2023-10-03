57ac4c994cf21b286f1476487eedfd4780c9db36
Zephic06 <70858757+Zephic06@users.noreply.github.com>
2023-10-03 08:54:56 +0200

    s

--- src/views/binair-stelsel.vue
+++ src/views/binair-stelsel.vue
@@ -1,6 +1,32 @@
 <template>
-    <main class="about-page">
+    <main class="binair-page">
+    <div class="binair-titel">
         <h1>Binair Stelsel</h1>
         <p>het binair stelsel uitgelegt en een embedded link naar Binary Bonanza voor verdere exploratie.</p>
+    </div>
+
+    <div class="binair-body"> 
+        <span>Het binaire stelsel, ook wel het tweetallige stelsel genoemd, is een wiskundig systeem dat gebruikt wordt voor het representeren en verwerken van informatie met behulp van slechts twee cijfers: 0 en 1. Dit in tegenstelling tot het decimale stelsel, dat gebruikt maakt van tien cijfers (0 tot 9). Het binaire stelsel vormt de basis van moderne digitale elektronica, inclusief computers en digitale communicatiesystemen, omdat het gemakkelijk kan worden geïmplementeerd met behulp van elektronische schakelingen waarin de aanwezigheid of afwezigheid van een elektrische spanning (1 of 0) wordt gebruikt om informatie op te slaan en te verwerken. </span>
+
+        <div class="binair-list">
+        <li>1. Binaire cijfers (bits) </li>
+        <button class="dropbtn">
+        <span class="material-symbols-outlined">expand_more</span>
+        </button>    
+        <li>2. Binair tellen</li> 
+        <button class="dropbtn">
+        <span class="material-symbols-outlined">expand_more</span>
+        </button>
+        <li>3. Binaire operaties</li> 
+        <button class="dropbtn">
+        <span class="material-symbols-outlined">expand_more</span>
+        </button>
+        <li>4. Data opslag</li> 
+        <button class="dropbtn"> 
+        <span class="material-symbols-outlined">expand_more</span>
+        </button>
+        </div>
+    </div>
+
     </main>
 </template>
